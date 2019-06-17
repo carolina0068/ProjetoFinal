85bf24a417250c17deff97219bcfaa8ea9a39cb5
Luis Bispo <luis.renato@live.com.pt>
2019-06-17 19:14:46 +0100

    Decomentação da Classe Stock.

--- src/projeto/Stock.java
+++ src/projeto/Stock.java
@@ -2,6 +2,12 @@ package projeto;
 
 import java.util.ArrayList;
 
+/**
+ * Esta classe serve para disponibilizar o stock de artigos que estão disponiveis
+ * @author Luis Bispo, José Boieiro, Carolina Timóteo
+ *
+ */
+
 public class Stock {
 	
 	// ATRIBUTOS
@@ -28,20 +34,33 @@ public class Stock {
 				
 				public Stock() {
 				}
-				
+				/**
+				 * Construtor do stock com os artigos disponiveis
+				 * @param stock dos artigos
+				 */
 				public Stock(ArrayList stock) {
 					Stock = stock;
 				}
 				
 	// METODOS
-				
+				/**
+				 * Serve para adicionar um artigo ao stock
+				 * @param artigo a ser adicionado ao stock
+				 */
 				public void add(Artigo artigo) {
 					Stock.add(0, artigo);
 				}
-				
+				/**
+				 * Serve para mostrar o tamanho atual do stock
+				 * @return Tamanho do stock
+				 */
 				public int size() {
 					return Stock.size();
 				}
+				/**
+				 * Serve para remover um artigo do stock
+				 * @param remove o artigo do stock
+				 */
 				public void remove() {
 					Stock.remove(0);
 				}
